cf15614517a7089488d131bf7c16750366377a2c
ellensp <530024+ellensp@users.noreply.github.com>
2022-06-05 01:33:49 +1200

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # gcode-thumbnailer
+
+A simple thumbnail generator for Gnome.
+This extracts the encoded thumbnail data from within a gcode file and turns it into OS useable thumbnail, such as in nautilus.
+
+Only tested on Ubuntu 20.04.4 LTS.
+
+Consists of two files:
+
+Gcode-with-thumbnail-thumbnailer.py
+The python script that extracts and generates the required png file.
+
+gcode-with-thumbnail.thumbnailer
+Configuration file to associate *.code with the thumbnailer application.
+
+Instiltion is manual:
+
+Copy gcode-with-thumbnail.thumbnailer to /usr/share/thumbnails/
+Copy gcode-with-thumbnail-thumbnailer.py to somewhere in you path. For eg /usr/local/bin/
+
+How to add thumbnail to gcode:
+
+I used the method mentioned here https://github.com/mriscoc/Ender3V2S1/wiki/How-to-generate-a-gcode-preview
---
+++ gcode-with-thumbnail-thumbnailer.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+#
+# gcode-with-thumbnail-thumbnailer.py - A Gnome 3 thumbnailer for gcode with an embedded thumbnail
+#
+#    Copyright (C) 2022 Peter Ellens <ellensp@hotmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/gpl>.
+
+#    # Contains code from the jpg re-encoder thumbnail post processor script:
+#    https://raw.githubusercontent.com/alexqzd/Marlin/Gcode-preview/Slicer%20post-processing%20scripts/PrusaSlicer_JPEG_Preview.py
+#    and  https://github.com/MestreLion/icns-thumbnailer
+
+import sys
+import re
+import os
+import base64 
+import io
+import subprocess
+
+try:
+    from PIL import Image
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "Pillow"])
+    from PIL import Image
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+def make_thumbnail(inputname, outputname, size=0):
+    try:
+        sourceFile = sys.argv[1]
+
+        # Read the ENTIRE g-code file into memory
+        with open(sourceFile, "r") as f:
+            lines = f.read()
+        f.close()
+
+        thumb_expresion = '; thumbnail begin.*?\n((.|\n)*?); thumbnail end'
+        size_expresion = '; thumbnail begin [0-9]+x[0-9]+ [0-9]+'
+        size_expresion_group = '; thumbnail begin [0-9]+x[0-9]+ ([0-9]+)'
+
+        thumb_matches = re.findall(thumb_expresion, lines)
+
+        if not thumb_matches:
+            sys.stderr.write("Thumbnail data not found\n")
+            return 1
+
+        for idx, match in enumerate(thumb_matches):
+           original = match[0]
+           encoded = original.replace("; ", "")
+           encoded = encoded.replace("\n", "")
+           encoded = encoded.replace("\r", "")
+           decoded = base64.b64decode(encoded)
+           pixbuf = Image.open(io.BytesIO(decoded))
+
+        if size:
+            width, height = pixbuf.size
+            if width > height:
+                if width > size:
+                    height = height * size / width
+                    width  = size
+            else:
+                if height > size:
+                    width  = width * size / height
+                    height = size
+
+            scaled = pixbuf.resize((int(width),int(height)), Image.ANTIALIAS)
+        else:
+            scaled = pixbuf
+
+        scaled.save(outputname)
+
+    except GLib.GError as e:
+        sys.stderr.write("%s:%d: %s\n" % (e.domain, e.code, e))
+        return e.code
+
+def main(argv):
+    try:
+        args = argv[1], argv[2], int((argv[3:4] or [0])[0])
+        if len(argv) > 4: raise IndexError
+    except (ValueError, IndexError):
+        sys.stderr.write("Usage: %s inputfile outputfile [size]\n" % argv[0])
+        return 1
+
+    return make_thumbnail(*args)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
